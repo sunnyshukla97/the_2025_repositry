96f15875106c3dcbd41bcef40dfc05ffca93b5d6
sunny shukla <shuklasunny1997@gmail.com>
2025-09-15 11:29:25 +0530

    First Commit

---
+++ js/one.js
@@ -0,0 +1,15 @@
+let person = {
+    name: "rajesh",
+    age: 25,
+    city: "Bangalore",
+    address :{
+        state : "Karnataka",
+        district : "Mysore",
+        pincode : 816107
+    },
+    hobbies : ["cricket", "movies", "reading", "driving"]
+}
+
+// console.log(person.address.district);
+console.log(person.hobbies[1]);
+
